139aa8615c3c8c42a31e3ef7df9a56235ebab8d3
Maarten Wallraf <mwallraf@2nms.com>
2020-12-04 19:14:22 +0100

    update cron entry

--- functions/entrypoint.sh
+++ functions/entrypoint.sh
@@ -7,6 +7,7 @@ echo "Docker container has been started"
 
 # Setup a cron schedule
 echo "0 6,20 * * * cd /opt/network-config-parser && /bin/bash /opt/network-config-parser/parser.sh --router-parser parse >> /var/log/cron.log 2>&1
+echo 0 8 * * * cd /opt/network-config-parser && /bin/bash /opt/network-config-parser/parser.sh --ces-parser parse >> /var/log/cron.log 2>&1
 @weekly rm -rf /var/log/cron.log
 # This extra line makes it a valid cron" > scheduler.txt
 
